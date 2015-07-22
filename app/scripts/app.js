5b0b4097e5cf3748ab0760d1e95b06bca6baec10
Karolinka <li-nee@gmx.de>
2015-07-22 17:20:51 +0200

    creating fakeBackend

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,7 @@
 
 angular
   .module('gumballApp', [
-    'ngAnimate', 'ngRoute'
+    'ngAnimate', 'ngRoute', 'ngMockE2E'
   ])
   .config(function ($routeProvider) {
      $routeProvider
@@ -17,4 +17,38 @@ angular
        .otherwise({
          redirectTo: '/'
        });
-   });
+   })
+  .run(function($httpBackend) {
+      var balls = [{
+                    "data": {
+                      "id": 12341,
+                      "type": "gumball",
+                      "attributes": {
+                        "color": "Blue",
+                        "flavor": "Bubble Gum",
+                        "description": "Yummy, yummy, Bubble Gummy! You got a Bubble Gum flavored gumball, you lucky dog."
+                      }
+                    }
+                  }, 
+                  {
+                  "data": {
+                    "id": 12342,
+                    "type": "gumball",
+                    "attributes": {
+                      "color": "Red",
+                      "flavor": "Bitter Tomato",
+                      "description": "Oh boy - everyone's favorite refreshing treat has rolled out of the machine and into your lucky hand!"
+                    }
+                  }
+                }];
+
+      // returns the current list of balls
+      $httpBackend.whenGET('/gumballs').respond(balls);
+
+      // adds a new ball to the balls array
+      $httpBackend.whenPOST('/gumballs').respond(balls);
+
+      $httpBackend.whenGET(/main/).passThrough();
+
+
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,23 @@
 'use strict';
 
 angular.module('gumballApp')
-  .controller('MainCtrl', [ 'loadData', '$http', function (loadData,$http) {
+  .controller('MainCtrl', [ 'loadData', '$http', '$scope', function (loadData,$http,$scope) {
 
-  	var url = 'gumballs/blue-gum-bubble.json';
+  	var url = '/gumballs';
+
+
+  	$scope.email = "heho"; 
+
 
   	$http.get(url).success(function(data){
   		console.log(data);
-
   	});
+
+  	$scope.sendData = function(message){
+  		$http.post(url, message).success(function(dataReturn){
+  			console.log(dataReturn);
+  		});
+  	};
 
 
   }]);
